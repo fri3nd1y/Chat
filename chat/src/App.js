9613e6470eae30223cd3f17b7c97bbdc0e947010
Binh Nguyen <Binh.dai.nguyen.97@gmail.com>
2020-09-24 04:44:58 -0700

    added firebase config

--- chat/src/App.js
+++ chat/src/App.js
@@ -8,6 +8,23 @@ import 'firebase/auth'
 import {useAuthState} from 'react-firebase-hooks/auth'
 import {useCollectionData} from 'react-firebase-hooks/firestore'
 
+import apiKey from '../config/apiKey'
+
+firebase.initializeApp({
+  apiKey: apiKey,
+  authDomain: "chat-d3d17.firebaseapp.com",
+  databaseURL: "https://chat-d3d17.firebaseio.com",
+  projectId: "chat-d3d17",
+  storageBucket: "chat-d3d17.appspot.com",
+  messagingSenderId: "930362018808",
+  appId: "1:930362018808:web:57bc60ed84ecb5c08ea40d",
+  measurementId: "G-6N1YCWZLMJ"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
+
 
 
 function App() {
